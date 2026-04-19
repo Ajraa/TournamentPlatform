46e514eb21180c5eb4a62cf7e3def90bc6043266
Pavel Salajka <salajkapavel@gmail.com>
2026-04-19 16:25:46 +0200

    Oprava TeamTypeRepository

--- spring-api/src/main/java/cz/ajraa/tournament/team/TeamTypeRepository.java
+++ spring-api/src/main/java/cz/ajraa/tournament/team/TeamTypeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TeamTypeRepository extends JpaRepository<Long, TeamTypeEntity> {
+public interface TeamTypeRepository extends JpaRepository<TeamTypeEntity, Long> {
     Optional<TeamTypeEntity> findByCode(String code);
 }
